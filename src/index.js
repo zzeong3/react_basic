4e1217acf73d7e2ce6c95fddb6ea3197fe8fd66d
zzeong3 <zzeong3@gmail.com>
2022-10-31 15:40:38 +0900

    index.js 에서 store 적용

--- src/index.js
+++ src/index.js
@@ -2,10 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import { HashRouter } from 'react-router-dom';
+import {Provider} from 'react-redux';
+import {configureStore} from '@reduxjs/toolkit';
+import youtubeReducer from './redux/youtubeSlice';
+
+const store = configureStore({
+  reducer  : {
+    yotube : youtubeReducer
+  }
+})
 
 ReactDOM.render(
 	<HashRouter>
-		<App />
+    <Provider store={store}>
+		  <App />
+    </Provider>
 	</HashRouter>,
 	document.getElementById('root')
 );
